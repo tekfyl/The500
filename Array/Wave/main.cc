cdce8532d681e945e7294b728e231b9c5c5b9d98
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-09-01 20:50:21 +0530

    AC

---
+++ Array/Wave/main.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define all(c) c.begin(), c.end()
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector< vi >
+#define vvl vector< vector<ll> >
+#define mk make_pair
+#define ii pair <int, int>
+#define LL pair <ll, ll>
+#define fi first
+#define se second
+#define mx max_element
+#define mn min_element
+#define rep(i,n)    for(__typeof(n) i = 0; i < n; i++)
+#define rep1(i,n)   for(__typeof(n) i = 1; i <= n; i++)
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+    vi v;
+    rep(i,n){
+        int in;
+        cin >> in;
+        v.pb(in);
+    }
+    sort(all(v));
+    for(int i=1; i<n; i+=2) if(i+1 < n) swap(v[i], v[i+1]);
+    for(int c:v) cout << c << " ";
+    return 0;
+}
+
