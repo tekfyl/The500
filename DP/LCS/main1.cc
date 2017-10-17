5b755196e676e7a19f14496c829b36d53b0b2ef7
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-10-17 13:30:50 +0530

    AC

---
+++ DP/LCS/main1.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void lcs(string str1, string str2){
+    int n = str1.length(), m = str2.length();
+    
+    vector<vector<int> > lookup(n+1, vector<int>(m+1, 0));
+    
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            if(str1[i-1] == str2[j-1]){
+                lookup[i][j] = lookup[i-1][j-1]+1;
+            }
+            else{
+                lookup[i][j] = max(lookup[i-1][j], lookup[i][j-1]);
+            }
+        }
+    }
+    
+    cout << lookup[n][m];
+}
+
+int main(){
+    string a = "ABCBDAB", b = "BDCABA";
+    lcs(a,b);
+    return 0;
+}
